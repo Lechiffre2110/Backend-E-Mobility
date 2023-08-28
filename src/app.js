9b08136c06f63f5b4781f1ebe0a19e2129d0c43a
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-08-28 20:33:57 +0200

    add endpoints

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
 require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const dataRoutes = require("./routes/dataRoutes");
+const contributorRoutes = require("./routes/contributorRoutes");
+const bugRoutes = require("./routes/bugRoutes");
+const onboardingRoutes = require("./routes/onboardingRoutes");
 const errorHandler = require("./middlewares/errorHandler");
+
 
 const app = express();
 const port = 5555;
 
+app.use(cors());
+
 //Set up logging
 app.use(morgan("combined"));
+
+app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
 app.use('/hub', dataRoutes);
+
+app.use('/api', contributorRoutes);
+
+app.use('/api', onboardingRoutes);
+
+app.use('/bug', bugRoutes);
+
+//TODO: change routes to make more sense
 
 app.use(errorHandler);
 
@@ -33,5 +50,5 @@ mongoose
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`E-Mobility backend listening on port ${port}`);
 });
