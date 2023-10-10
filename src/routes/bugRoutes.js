c718507c41d5183ad32f991a545190d1710b1a12
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-10-10 15:08:01 +0200

    remove unused routes

--- src/routes/bugRoutes.js
+++ src/routes/bugRoutes.js
@@ -7,6 +7,5 @@ const bugController = require("../controllers/bugController");
 router.post("/", upload.none(), bugController.addBug);
 router.get("/", bugController.getBugs);
 router.put("/:id/resolve", upload.none(), bugController.resolveBug);
-router.put("/:id", upload.none(), bugController.editBug);
 
 module.exports = router;
